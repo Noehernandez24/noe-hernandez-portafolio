38d0c3b30f64c76890d7c5e5f15e316a30f9fe68
noehernandez24 <evilld94@gmail.com>
2023-02-12 20:02:54 -0600

    FIX: More btn 2

--- src/components/MoreBtn.jsx
+++ src/components/MoreBtn.jsx
@@ -4,7 +4,7 @@ const MoreBtn = ({changeSkillPage, currentPage, totalPages}) => {
     const arrowDirection = currentPage >= 1 && currentPage < totalPages ? 'rotate-0' : 'rotate-180'
   return (
     <span
-      className="down text-center cursor-pointer right-auto lg:block absolute bottom-14 sm:top-1/2 sm:right-28 lg:right-32 2xl:right-80"
+      className="down text-center cursor-pointer right-auto lg:block sm:absolute bottom-14 sm:top-1/2 sm:right-28 lg:right-32 2xl:right-80"
       onClick={changeSkillPage}
     >
       <i className={`bx bx-chevron-right  ${arrowDirection} bx-md border rounded-full bg-white text-black transition ease-in duration-200`}></i>
